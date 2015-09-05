3076a4f603310756691bca4dcc5185704a5df65d
pyld <lidongone@qq.com>
2015-09-05 23:43:52 +0800

    1

--- rules.py
+++ rules.py
@@ -7,12 +7,53 @@ from html import unescape
 
 thisday = datetime.datetime.today().strftime('%Y-%m-%d')
 
+def huxiu_pyld():
+    '''虎嗅网-看点'''
+    my_title = huxiu_pyld.__doc__
+    column = 6
+    iscover = 1
+    try:
+        s = requests.Session()
+        today1 = datetime.datetime.today().strftime('%Y-%m-%d')
+        # 这个if用来决定抓哪天的
+        # if datetime.datetime.today().hour < 5:
+        #     today1 = '%s-%s' % (datetime.datetime.today().strftime('%m'),
+        #                         (datetime.datetime.today().day - 1))
+        pagenum = 1
+        aa = []
+        while 1:
+            r = s.get('http://www.huxiu.com/focus/%s.html' % pagenum)
+            scode = r.content.decode('utf-8')
+            xpath = fromstring(scode).xpath
+            dates=[re.sub(' .*','',i) for i in xpath('//time[@class="time"]/text()')]
+            if today1 not in dates:
+                break
+            pagenum += 1
+            urls = ['http://www.tuicool.com/articles/' + i for i in xpath('//div[@class="clearfix shadow-box-noshadow mod-info-flow"]//h3/a/@href')]
+            covers = xpath('//div[@class="clearfix mod-b mod-art"]/a/img/@src')
+            titles = xpath('//div[@class="clearfix shadow-box-noshadow mod-info-flow"]//h3/a/text()')
+            desc = [i.strip()
+                    for i in xpath('//div[@class="mob-sub"]/text()')]
+            ptime = xpath('//div[@class="mob-author"]')
+            ptime = ['<div style="font-size:15px;" align="right"><br>%s</div>' % re.sub('\s{2,}', '&nbsp&nbsp&nbsp&nbsp', i.text_content().replace('稍后阅读', '').strip())
+                     for i in ptime]
+            # print(ptime)
+            desc = ['<br>'.join(i) for i in list(zip(desc, ptime))]
+            aa += list(zip(covers, titles, urls, desc))
+        # print('推酷——finished……')
+    except Exception as e:
+        print(e)
+        aa = [['error'] * 4]
+    print(my_title, 'finished')
+    return [my_title, aa, column, iscover]
+
+
 
 def iplaysoft_pyld():
     '''异次元软件世界（RSS）'''
     my_title = iplaysoft_pyld.__doc__
     column = 6
-    iscover = 0
+    iscover = 1
     try:
         r = requests.get('http://feed.iplaysoft.com/')
         ss = unescape(r.text)
