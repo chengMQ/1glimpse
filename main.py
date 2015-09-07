52c4e9bcab0cab32997d9e67b796ff7d1f256a32
pyld <lidongone@qq.com>
2015-09-07 21:58:26 +0800

    1

---
+++ main.py
@@ -0,0 +1,87 @@
+import re
+from multiprocessing.dummy import Pool
+import datetime
+import time
+import os
+import rules
+from inspect import getmembers
+
+
+this_day = datetime.datetime.now().strftime('%Y-%m-%d')
+this_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+def list2html(table_html):
+    # 具体的数据转HTML过程
+    website, list1, num, iscover = table_html
+    head = "<div sitename='{}'><table cellspacing=5 cellpadding=7 width='100%' align='center'><tbody><p><h2 align='center'>{}</h2></p>blablablabla</tbody></table></div>".format(
+        website,website)
+    if not list1:
+        return head.replace('blablablabla','<div align="center">暂无新数据</div>') 
+    table1 = ''
+    for i, each in enumerate(list1):
+        cover, title, url, desc = each
+        if iscover:
+            cover = '<img   width="100%" height=200  src="{}"  />'.format(
+                cover)
+        else:
+            cover = ''
+        n = i + 1
+        if (n - 1) % num == 0:
+            table1 += '<tr style="width:100%;">'
+        table1 += '<td style="background:#cccccc" VALIGN=TOP width="{width1}%"><a  href="{url}" target="_blank" style="font-size:18px;">{cover}<p><span style="color:#000000;"><strong>{title}</strong></span></p></a><span style="font-size:16px;">{desc}</span></td>'.format(
+            cover=cover, url=url, title=title, desc=desc, width1=100 / num)
+        if n % num == 0 or n == len(list1):
+            table1 += '</tr>'
+    return head.replace('blablablabla',table1) 
+
+
+def withpics_to_file(ss):
+    # 传入多个feed的列表，输出为文件
+    ss = [list2html(i) for i in ss]
+    with open('./pages/%s[cover-yes].html' % this_day, 'w', encoding='utf-8') as f:
+
+        scode = '<meta charset="utf-8"><style> body{background-color:#a4a4a4} </style><style>A {text-decoration: NONE} </style><div align="right">采集时间：%s</div><hr>' % (this_time) + '<br>'.join(
+            ss)
+        f.write(scode)
+
+
+def withoutpic_to_file(ss):
+    for i in ss:
+        i[-1]=0
+    ss = [list2html(i) for i in ss]
+    with open('./pages/%s[cover-no].html' % this_day, 'w', encoding='utf-8') as f:
+
+        scode = '<meta charset="utf-8"><style> body{background-color:#a4a4a4} </style><style>A {text-decoration: NONE} </style><div align="right">采集时间：%s</div><hr>' % (this_time) + '<br>'.join(
+            ss)
+        f.write(scode)
+
+
+def refresh_index():
+    # 刷新index.html
+    def list2index(each_item):
+        return '<li><a target="_blank" style="font-size:18px;" href="./pages/%s.html">%s</a></li>' % (each_item, each_item + 'blabla')
+    list0 = [list2index(i.replace('.html', '')) for i in sorted(
+        os.listdir('.\\pages'), reverse=True) if i.endswith('html')]
+    list1=''.join([i.replace('[cover-yes]blabla','') for i in list0 if '[cover-yes]' in i])
+    list2=''.join([i.replace('[cover-no]blabla','') for i in list0 if '[cover-no]' in i])
+    str1 = '<!--网址列表--start--><p style="font-size:18px;">最后更新：{}</p><p style="font-size:18px;"><strong>当前订阅列表：</strong>{}</p><div align="center"><table style="width:100%;" border="0" cellpadding="2" cellspacing="0" align="center"><tbody><tr><td><strong style="font-size:35px;">有图版</strong></td><td><strong style="font-size:35px;">无图版</strong></td></tr><tr><td><ul>{}</ul></td><td><ul>{}</ul></td></tr></tbody></table></div><!--网址列表--end-->'.format(this_time,titles,list1,list2)
+    # print(str1)
+    with open('index.html', 'r', encoding='utf-8') as f:
+        scode = f.read()
+    with open('index.html', 'w', encoding='utf-8') as f:
+        f.write(
+            re.sub('<!--网址列表--start-->[\s\S]+?<!--网址列表--end-->', str1, scode))
+
+if __name__ == '__main__':
+    func_names = dict([i for i in getmembers(rules) if i[0].endswith('pyld')])
+    func_list = sorted(func_names.keys())
+    choose_func = [func_names[i] for i in func_list]
+    pp = Pool(20)
+    ss = pp.map(lambda x: x(), choose_func)
+    pp.close()
+    pp.join()
+    titles = '、'.join(['[%s]' % i[0] for i in ss])
+    withpics_to_file(ss)
+    withoutpic_to_file(ss)
+    refresh_index()
---
+++ rules.py
@@ -0,0 +1,153 @@
+import requests
+import re
+from lxml.html import fromstring
+import datetime
+import time
+from html import unescape
+
+thisday = datetime.datetime.today()
+
+
+def huxiu_pyld():
+    '''虎嗅网-看点'''
+    my_title = huxiu_pyld.__doc__
+    column = 6
+    iscover = 1
+    try:
+        s = requests.Session()
+        today1 = thisday.strftime('%Y-%m-%d')
+        # 这个if用来决定抓哪天的
+        # if datetime.datetime.today().hour < 5:
+        #     today1 = '%s-%s' % (datetime.datetime.today().strftime('%m'),
+        #                         (datetime.datetime.today().day - 1))
+        pagenum = 1
+        aa = []
+        while 1:
+            r = s.get('http://www.huxiu.com/focus/%s.html' % pagenum)
+            scode = r.content.decode('utf-8')
+            xpath = fromstring(scode).xpath
+            dates = [re.sub(' .*', '', i)
+                     for i in xpath('//time[@class="time"]/text()')]
+            if today1 not in dates:
+                break
+            pagenum += 1
+            urls = ['http://www.tuicool.com/articles/' + i for i in xpath(
+                '//div[@class="clearfix shadow-box-noshadow mod-info-flow"]//h3/a/@href')]
+            covers = xpath('//div[@class="clearfix mod-b mod-art"]/a/img/@src')
+            titles = xpath(
+                '//div[@class="clearfix shadow-box-noshadow mod-info-flow"]//h3/a/text()')
+            desc = [i.strip()
+                    for i in xpath('//div[@class="mob-sub"]/text()')]
+            ptime = xpath('//div[@class="mob-author"]')
+            ptime = ['<div style="font-size:15px;" align="right"><br>%s</div>' % re.sub('\s{2,}', '&nbsp&nbsp&nbsp&nbsp', i.text_content().replace('稍后阅读', '').strip())
+                     for i in ptime]
+            # print(ptime)
+            desc = ['<br>'.join(i) for i in list(zip(desc, ptime))]
+            aa += list(zip(covers, titles, urls, desc))
+        # print('推酷——finished……')
+    except Exception as e:
+        print(e)
+        aa = [['error'] * 4]
+    print(my_title, 'finished')
+    return [my_title, aa, column, iscover]
+
+
+def appinn_pyld():
+    '''小众软件（RSS）'''
+    my_title = appinn_pyld.__doc__
+    column = 7
+    iscover = 1
+    try:
+        r =  requests.get('http://feeds.appinn.com/appinns/')
+        ss = unescape(r.text)
+        xpath = fromstring(re.sub('<.*?>', '', ss, 1)).xpath
+        titles = xpath('//item/title/text()')
+        covers = re.findall('<content:encoded>[\s\S]*?<img.*?src="(.*?)"',ss)
+        desc = xpath('//description/text()')[1:]
+        urls = re.findall('<link>(.*?)</link>',ss)[1:]
+        ptime = ['<div align="right"><br>%s</div>' % datetime.datetime.strptime(i, '%a, %d %b %Y %H:%M:%S GMT').strftime(
+            '%Y-%m-%d %H:%M:%S') for i in xpath('//pubdate/text()')[1:]]
+        desc = [''.join(i) for i in list(zip(desc, ptime))]
+        result = list(zip(covers, titles, urls, desc))[:14]
+    except:
+        result = [['error'] * 4]
+        # print('异次元软件世界——finished……')
+    print(my_title, 'finished')
+    return [my_title, result, column, iscover]
+
+
+def iplaysoft_pyld():
+    '''异次元软件世界（RSS）'''
+    my_title = iplaysoft_pyld.__doc__
+    column = 6
+    iscover = 1
+    try:
+        r = requests.get('http://feed.iplaysoft.com/')
+        ss = unescape(r.text)
+        xpath = fromstring(re.sub('<.*?>', '', ss, 1)).xpath
+        titles = [i.replace('[来自异次元]', '').strip()
+                  for i in xpath('//item/title/text()')]
+        covers = xpath('//item/description/p[1]/a/img/@src')
+        desc = [i.text_content()
+                for i in xpath('//item/description/div[1]/p[1]')]
+        urls = xpath('//item/description/p[1]/a/@href')
+        ptime = ['<div align="right"><br>%s</div>' % datetime.datetime.strptime(i, '%a, %d %b %Y %H:%M:%S GMT').strftime(
+            '%Y-%m-%d %H:%M:%S') for i in xpath('//pubdate/text()')[1:]]
+        desc = [''.join(i) for i in list(zip(desc, ptime))]
+        result = list(zip(covers, titles, urls, desc))[:6]
+    except:
+        result = [['error'] * 4]
+        # print('异次元软件世界——finished……')
+    print(my_title, 'finished')
+    return [my_title, result, column, iscover]
+
+
+def tuicool_pyld():
+    '''推酷-文章'''
+    my_title = tuicool_pyld.__doc__
+    column = 6
+    iscover = 1
+    try:
+        s = requests.Session()
+        today1 = thisday.strftime('%m-%d')
+        # 这个if用来决定抓哪天的
+        # if datetime.datetime.today().hour < 5:
+        #     today1 = '%s-%s' % (datetime.datetime.today().strftime('%m'),
+        #                         (datetime.datetime.today().day - 1))
+        pagenum = 0
+        aa = []
+        while 1:
+            r = s.get('http://www.tuicool.com/ah/0/%s?lang=1' % pagenum)
+            scode = r.text
+            xpath = fromstring(scode).xpath
+            dates = re.findall(
+                '<i class="icon-time icon"></i>(.*?)</span>', scode, flags=re.S)
+            date = re.findall('(\d\d-\d\d)\D', ' '.join(dates))
+            if today1 not in date:
+                break
+            pagenum += 1
+            pids = xpath('//div[@class="single_fake"]/@data-id')
+            urls = ['http://www.tuicool.com/articles/' + i for i in pids]
+            covers = [xpath(
+                '//div[@data-id="%s"]//div[@class="article_thumb"]/img/@src' % i) for i in pids]
+            covers = list(map(lambda x: x[0].replace(
+                '!middle', '') if x else 'empty.jpg', covers))
+            titles = xpath('//a[@class="article-list-title"]/text()')
+            desc = [i.strip()
+                    for i in xpath('//div[@class="article_cut"]/text()')]
+            ptime = xpath('//div[@class="tip meta-tip"]')
+            ptime = ['<div style="font-size:15px;" align="right"><br>%s</div>' % re.sub('\s{2,}', '&nbsp&nbsp&nbsp&nbsp', i.text_content().replace('稍后阅读', '').strip())
+                     for i in ptime]
+            # print(ptime)
+            desc = ['<br>'.join(i) for i in list(zip(desc, ptime))]
+            aa += list(zip(covers, titles, urls, desc))
+        # print('推酷——finished……')
+    except Exception as e:
+        print(e)
+        aa = [['error'] * 4]
+    print(my_title, 'finished')
+    return [my_title, aa, column, iscover]
+
+
+if __name__ == '__main__':
+    print('请使用其他模块进行调用')
