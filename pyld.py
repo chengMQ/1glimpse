406a090375e35b1501a1c1c7f4f7173f64b4afb1
ClericPy <1>
2015-09-20 16:39:58 +0800

    General update

--- pyld.py
+++ pyld.py
@@ -1,6 +1,11 @@
-import os
-os.chdir(r'D:\kuaipan\1glimpse\ClericPy.github.io')
-os.system('git-bash.exe')
-# os.system('git add *')
-# os.system('git commit -m "常规更新"')
-# os.system('git push origin master')
+from ahk import goahk
+
+goahk('''run,"C:\Program Files\Git\git-bash.exe"
+Sleep, 1000
+WinWait, ahk_class mintty, 
+Sleep, 1000
+ControlSend , , git add * -f{Enter}, ahk_class mintty
+Sleep, 800
+ControlSend , , git commit -m "general update"{Enter}, ahk_class mintty
+Sleep, 800
+ControlSend , , git push origin master{Enter}, ahk_class mintty''')
