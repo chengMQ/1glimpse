3bad5c82777446fb5ead79d188d1b102ff0f9293
ClericPy <1>
2015-09-22 23:36:15 +0800

    常规更新

---
+++ upload.py
@@ -0,0 +1,7 @@
+import os
+# os.chdir('./ClericPy.github.io')
+os.system('python main.py')
+os.system(r'git add * -f')
+os.system(r'git commit -m "常规更新"')
+os.system(r'git push origin master')
+os.system('pause')
