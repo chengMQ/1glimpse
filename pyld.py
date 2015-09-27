56a9e2c0ddfe3d780a028f851c2276a00c6b750b
ClericPy <1>
2015-09-27 23:37:24 +0800

    常规更新

--- pyld.py
+++ pyld.py
@@ -8,56 +8,34 @@ from html import unescape
 thisday = datetime.datetime.today()
 
 
-def pyld_tuicool():
-    '''<a style="color:#000000;" target="_blank" href="http://www.tuicool.com/ah" title="推酷网是面向IT人的个性化阅读网站,其背后的推荐引擎通过智能化的分析,向用户推荐感兴趣的科技资讯、产品设计、网络营销、技术文章等内容。它最大的收录价值在于，不但汇聚了当前主流IT资讯类网站的内容，并且在其中进行了精选，省去了浏览冷门知识的时间。">推酷-文章</a>'''
+def pyld_pythondaily():
+    '''<a style="color:#000000;" target="_blank" href="http://forum.memect.com/blog/thread-category/py/" title="好东西传送门旗下python干货合集，一日一更，虽然订阅了邮件，但还是想留点存档看看。">好东西传送门-python日报</a>'''
     starttime = time.time()
-    my_title = pyld_tuicool.__doc__
+    my_title = pyld_pythondaily.__doc__
     title_clean = re.sub('<.*?>', '', my_title)
-    column = 6
-    iscover = 1
-    try:
-        s = requests.Session()
-        today1 = thisday.strftime('%m-%d')
-        # 这个if用来决定抓哪天的
-        # if datetime.datetime.today().hour < 5:
-        #     today1 = '%s-%s' % (datetime.datetime.today().strftime('%m'),
-        #                         (datetime.datetime.today().day - 1))
-        pagenum = 0
-        aa = []
-        while 1:
-            r = s.get('http://www.tuicool.com/ah/0/%s?lang=1' % pagenum)
-            scode = r.text
-            xpath = fromstring(scode).xpath
-            dates = re.findall(
-                '<i class="icon-time icon"></i>(.*?)</span>', scode, flags=re.S)
-            date = re.findall('(\d\d-\d\d)\D', ' '.join(dates))
-            if today1 not in date:
-                break
-            pagenum += 1
-            pids = xpath('//div[@class="single_fake"]/@data-id')
-            urls = ['http://www.tuicool.com/articles/' + i for i in pids]
-            covers = [xpath(
-                '//div[@data-id="%s"]//div[@class="article_thumb"]/img/@src' % i) for i in pids]
-            # covers = [i[0].replace(
-            #     '!middle', '') for i in covers if i else '']
-            #.replace('!middle', '')
-            covers = list(
-                map(lambda x: re.sub('!middle$', '', x[0]) if x else '', covers))
-            titles = xpath('//a[@class="article-list-title"]/text()')
-            sums = [i.strip()
-                    for i in xpath('//div[@class="article_cut"]/text()')]
-            ptime = xpath('//div[@class="tip meta-tip"]')
-            ptime = ['<div style="font-size:15px;" align="right"><br>%s</div>' % re.sub('\s{2,}', '&nbsp&nbsp&nbsp&nbsp', i.text_content().replace('稍后阅读', '').strip())
-                     for i in ptime]
-            # print(ptime)
-            sums = ['<br>'.join(i) for i in list(zip(sums, ptime))]
-            aa += list(zip(covers, titles, urls, sums))
+    column = 6  # 根据内容数量来划分
+    iscover = 0
+    for day1 in range(3):
+        try:
+            theday = (
+                thisday - datetime.timedelta(days=day1)).strftime("%Y-%m-%d")
+            url = 'http://forum.memect.com/blog/thread/py-%s/' % theday
+            r = requests.get(url)
+            if r.status_code==404:
+                continue
+            items = [tostring(i,encoding='utf-8').decode('utf-8') for i in fromstring(r.text).xpath('//div[@id="container"]/div[@class]')]
+            sums = [re.sub('<img.*?>','',i) for i in items]
+            urls = [''] * len(sums)
+            titles = [''] * len(sums)
+            covers = [''] * len(sums)
+            aa = list(zip(covers, titles, urls, sums))
 
-    except Exception as e:
-        print('%s  %s' % (title_clean, e))
-        aa = [['error'] * 4]
-    runtime1 = round(time.time() - starttime, 3)
-    print(title_clean, 'finished in %s seconds' % runtime1)
-    aa = [i for i in aa if thisday.strftime('%m-%d') in i[3]]
-    return [my_title, aa, column, iscover]
-print(pyld_tuicool())
+        except Exception as e:
+            print('%s  %s' % (title_clean, e))
+            aa = [['error'] * 4]
+        runtime1 = round(time.time() - starttime, 3)
+        print(title_clean, 'finished in %s seconds' % runtime1)
+        return [my_title, aa, column, iscover]
+
+
+pyld_pythondaily()
