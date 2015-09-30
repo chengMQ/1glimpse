7c0b8e431b15a08a6428f59679d05b55b572be2a
ClericPy <1>
2015-10-01 02:14:09 +0800

    常规更新

--- upload.py
+++ upload.py
@@ -1,11 +1,7 @@
 import os
-import time
 # os.chdir('./ClericPy.github.io')
 os.system('python main.py')
 os.system(r'git add * -f')
 os.system(r'git commit -m "常规更新"')
 os.system(r'git push origin master')
-# os.system('pause')
-for i in range(1,4):
-    print(4-i)
-    time.sleep(1)
+os.system('pause')
