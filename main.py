ed6d4e6f0d8d24eaf1a869c3488528dadd995173
ClericPy <1>
2015-12-05 13:27:44 +0800

    常规更新

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ def withpics_to_file(ss):
         f.write(scode)
     # for mobile
     with open('./pages/%s[cover-yes-mobile].html' % this_day, 'w', encoding='utf-8') as f:
-        scode = re.sub('<td.*?>|</td>', '<hr>', scode).replace('font-size:16px;',
-                                                           'font-size:46px;').replace('font-size:18px;', 'font-size:58px;')
+        scode = re.sub('<td.*?>|</td>', '<fengebiaoji>', scode).replace('font-size:16px;',
+                                                           'font-size:46px;').replace('font-size:18px;', 'font-size:58px;').replace('<fengebiaoji><fengebiaoji>', '<hr>')
         f.write(scode)
 
 
